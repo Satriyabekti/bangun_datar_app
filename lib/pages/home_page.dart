b975ae1fd2ef4b2387eccdd0af476b100b8f5c75
USER <sutarnos470@gmail.com>
2023-11-14 14:41:12 +0700

    custom component

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -15,24 +15,50 @@ class HomePage extends StatelessWidget {
             )),
         body: Column(
           children: [
+            CustomMenu(imageAssets: "asset/images.png",title: "Persegi"),
+            CustomMenu(imageAssets: "asset/png_uw3a9g_3328.png",title: "Lingkaran"),
             Container(
               margin: EdgeInsets.all(8),
               padding: EdgeInsets.symmetric(vertical:8, horizontal:16),
               decoration: BoxDecoration(
                   color: Colors.green
               ),
-              child: Text("Persegi"),
-            ),
-            Container(
-              margin: EdgeInsets.all(8),
-              padding: EdgeInsets.symmetric(vertical:8, horizontal:16),
-              decoration: BoxDecoration(
-                  color: Colors.green
+              child: Column(
+                children: [
+                  Image.asset("asset/png_uw3a9g_3328.png", height: 100,),
+                  Text("Lingkaran"),
+                ],
               ),
-              child: Text("Persegi"),
             ),
           ],
         )
+    );
+  }
+}
+
+class CustomMenu extends StatelessWidget {
+  const CustomMenu({
+    super.key, required this.imageAssets, required this.title,
+  });
+  final String imageAssets;
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(8),
+      padding: EdgeInsets.symmetric(vertical:8, horizontal:16),
+      decoration: BoxDecoration(
+          color: Colors.green
+      ),
+      child: Column(
+        children: [
+          Image.asset(
+            imageAssets
+          ),
+          Text("Persegi"),
+        ],
+      ),
     );
   }
 }
